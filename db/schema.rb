7610fcdc328c9e5b8694dd889e4df157b313dc86
Nathan Peters <peters.nate8@gmail.com>
2024-03-26 12:48:32 -0600

    add content and completed attributes to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_26_183234) do
+ActiveRecord::Schema.define(version: 2024_03_26_184333) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
+    t.boolean "completed", default: false
   end
 
 end
